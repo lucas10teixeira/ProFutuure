61066c632ef0c02f9ac72ba11f24f03a0d277187
LUCAS TEIXEIRA CARDOSO <03905498006@senacrs.edu.br>
2024-10-30 20:42:29 -0300

    ajustes no feed

--- src/routes/feedRouter.js
+++ src/routes/feedRouter.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { storefeed } = require('../controller/feedController');
- 
+const { getposts } = require('../controller/feedController');
+const { deletepost } = require('../controller/feedController');
 // Rota para criar um novo post no feed
 router.post('/store/feed', storefeed);
-/**
-* @swagger
-* /feed/feed:
-*   post:
-*     summary: postagens do feed
-*     responses:
-*       200:
-*         description: postagens do feed
-*         content:
-*           application/json:
-*             schema:
-*               type: array
-*               items:
-*                 type: object
-*/
- 
+router.get('/feed/getposts', getposts)
+
+router.delete('/feed/deletepost/:id', deletepost);
+
 module.exports = router;
