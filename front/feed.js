1842f31122056f895d02634cd7126fe5de4783dd
Lucas <lucasteixeira1507@gmail.com>
2024-11-06 20:46:19 -0300

    Alert com informações do usuario 06/11

--- front/feed.js
+++ front/feed.js
@@ -1,13 +1,15 @@
 function excluirPostagem(id) {
-    const userId = localStorage.getItem('userId');
-    const postId = id;
-  
-    fetch(`http://localhost:3001/api/feed/deletepost/${postId}`, {
+  const userId = localStorage.getItem('userId');
+  const postId = id;
+
+  fetch(`http://localhost:3001/api/feed/deletepost/${postId}`, {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({ userId: userId })
+      body: JSON.stringify({
+        userId: userId
+      })
     })
     .then(response => response.json())
     .then(data => {
@@ -21,63 +23,99 @@ function excluirPostagem(id) {
         Swal.fire('Erro ao excluir postagem!', '', 'error');
       }
     })
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const feedContainer = document.getElementById('feed');
+  const userId = localStorage.getItem('userId'); // Obter o ID do usuário
+
+  loadPosts(); // Carregar os posts ao inicializar a página
+  if (!userId) {
+    console.error('ID do usuário não encontrado.');
+    return;
   }
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    const feedContainer = document.getElementById('feed');
-    const userId = localStorage.getItem('userId'); // Obter o ID do usuário
-  
-    loadPosts(); // Carregar os posts ao inicializar a página
-    if (!userId) {
-      console.error('ID do usuário não encontrado.');
-      return;
-    }
-  
-    async function loadPosts() {
-      try {
-        const response = await fetch('http://localhost:3001/api/feed/getposts');
-        const data = await response.json();
-    
-        if (data.success) {
-          for (const post of data.data) {
-            addPost({
-              descricao: post.descricao,
-              username: post.username,
-              id: post.id,
-              userId: post.user_id.toString() // Convertendo para string para garantir a comparação correta
-            });
-          }
-        } else {
-          console.error('Erro ao carregar postagens:', data.message);
+
+  async function loadPosts() {
+    try {
+      const response = await fetch('http://localhost:3001/api/feed/getposts');
+      const data = await response.json();
+
+      if (data.success) {
+        for (const post of data.data) {
+          addPost({
+            descricao: post.descricao,
+            username: post.username,
+            id: post.id,
+            userId: post.user_id.toString() // Convertendo para string para garantir a comparação correta
+          });
         }
-      } catch (error) {
-        console.error('Erro ao carregar postagens:', error);
+      } else {
+        console.error('Erro ao carregar postagens:', data.message);
       }
+    } catch (error) {
+      console.error('Erro ao carregar postagens:', error);
     }
-  
-    function addPost(post) {
-      if (!document.getElementById(`post-${post.id}`)) {
-        const postElement = document.createElement('div');
-        postElement.classList.add('post');
-        postElement.id = `post-${post.id}`;
-        
-        const loggedInUserId = localStorage.getItem('userId');
-        
-        let postContent = `
-          <p class="post-username"><strong>${post.username}</strong></p>
+  }
+
+  function addPost(post) {
+    if (!document.getElementById(`post-${post.id}`)) {
+      const postElement = document.createElement('div');
+      postElement.classList.add('post');
+      postElement.id = `post-${post.id}`;
+
+      const loggedInUserId = localStorage.getItem('userId');
+
+      let postContent = `
+          <p class="post-username" onclick="sobre(${post.userId})"><strong>${post.username}</strong></p>
           <p class="post-content">${post.descricao}</p>
         `;
-        
-        if (post.userId === loggedInUserId) {
-          postContent += `
+
+      if (post.userId === loggedInUserId) {
+        postContent += `
             <button class="delete-btn" onclick="excluirPostagem(${post.id})">
               <i class="bi bi-trash3-fill"></i>
             </button>
           `;
-        }
-        
-        postElement.innerHTML = postContent;
-        feedContainer.prepend(postElement);
+      }
+
+      postElement.innerHTML = postContent;
+      feedContainer.prepend(postElement);
+    }
+  }
+});
+async function sobre(userId) {
+  try {
+    const response = await fetch(`http://localhost:3001/api/user/${userId}`);
+    if (!response.ok) {
+      throw new Error('Erro ao obter dados do usuário');
+    }
+
+    const userData = await response.json();
+    console.log(userData);
+
+    const nomeUser = userData.data.nome;
+    const sobreUser = userData.data.sobre;
+
+    // Verifica se os dados do usuário foram recebidos com sucesso
+    if (userData.success && userData.data) {
+      if (userData.data.nome) {
+      
+        Swal.fire({
+          title: `<strong><h2>Usuário:<h2>${nomeUser}</strong>`,
+          html: `<p><h2>Sobre:<h2>${sobreUser}</p>`,
+          icon: 'info',
+          confirmButtonText: 'Fechar',
+          customClass: {
+            title: 'swal-title',
+            popup: 'swal-popup',
+            confirmButton: 'swal-confirm-button'
+          }
+        });
+      } else {
+        console.error('Nome do usuário não encontrado nos dados recebidos:', userData);
       }
     }
-  });
+  } catch (error) {
+    console.error('Erro ao buscar dados do usuário:', error);
+  }
+}
